308a58df4c0682a552f2219bfb842fb4164acc8a
Bartosz Kobylinski <bartosz.kobylinski@gmail.com>
2023-01-18 21:50:06 +0100

    Adding the attribute 'uniqueID' to the 'airplane' class to differentiate particular airplanes.

--- plane_new.py
+++ plane_new.py
@@ -1,8 +1,11 @@
 import socket 
 import json
+import random
+import string
 
 
 class Airplane(socket.socket):
+    unique_ids = set()
 
     def __init__ (self, host='127.0.0.1', port=9485, encoder='utf-8', buffer=1024):
         self.host = host
@@ -11,9 +14,13 @@ class Airplane(socket.socket):
         self.buffer = buffer
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.host, self.port))
+        self.uniqueID = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+        while self.uniqueID in Airplane.unique_ids:
+            self.uniqueID = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+        Airplane.unique_ids.add(self.uniqueID)
 
     def send_json(self, data):
-        json_data = json.dumps(data)
+        json_data = json.dumps(f"{self.uniqueID}: {data}")
         self.socket.send(json_data.encode(self.encoder))
         
     def recv_json(self):
