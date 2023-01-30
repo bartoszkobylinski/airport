0d1239e92dadbe6816125a273952d5df203de1a6
Bartosz Kobylinski <bartosz.kobylinski@gmail.com>
2023-01-30 14:11:50 +0100

    Adding a flag to close server connection properly

--- main_airport.py
+++ main_airport.py
@@ -1,31 +1,41 @@
 import random
+import threading
+import time
+from airport_class import Airport
 
-IP_PORT = random.randint(5000, 20000)
-'''
-import numpy as np
+start_time = time.time()
 
-class Airport(Socket_Connection):
-    # existing code here
+current_time = time.time()
+delta_time = current_time - start_time
 
-    def simulate_flight(self):
-        while True:
-            for airplane in Airport.airplanes:
-                distance = np.linalg.norm(np.array(
-                    [airplane.x, airplane.y, airplane.z]) 
-                    - np.array([self.corridor_1_x, self.corridor_1_y, self.corridor_1_z]))
-                print(f'Airplane {airplane.id} is {distance} meters away from the corridor.')
-                if distance <= 100:
-                    print(f'Airplane {airplane.id} has entered the corridor.')
-                    Airport.airplanes.remove(airplane)
-                else:
-                    direction_vector = (np.array([self.corridor_1_x, self.corridor_1_y, self.corridor_1_z]) - 
-                        np.array([airplane.x, airplane.y, airplane.z])) / distance
-                    airplane.x += airplane.velocity * direction_vector[0]
-                    airplane.y += airplane.velocity * direction_vector[1]
-                    airplane.z += airplane.velocity * direction_vector[2]
-                    airplane.fuel -= 1
-                    if airplane.fuel == 0:
-                        print(f'Airplane {airplane.id} has run out of fuel and must land.')
-                        Airport.airplanes.remove(airplane)
-'''
+airport = Airport()
+lock = threading.Lock()
 
+threads = []
+server_running = True
+
+while delta_time < 3600:
+    current_time = time.time()
+    print("Waititng for the incoming connections")
+    print("-------------------------------------")
+    try:
+        client_socket, adrres = airport.socket.accept()
+    except Exception as e:
+        print(f"Server closed connection")
+        server_running = False
+        break
+    try:
+        t = threading.Thread(target=airport.handle_new_client, args=[client_socket])
+        t.start()
+        threads.append(t)
+    except Exception as e:
+        print(f"I try start new thread but i got Excpetion: {e}")
+        server_running = False
+        break
+
+    delta_time = current_time - start_time
+
+for t in threads:
+    t.join()
+if not server_running:
+    airport.socket.close()
