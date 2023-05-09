d8051cc0c5213fdba90612fce6770dc211a113e3
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-05-09 14:38:59 +0200

    Title: Implement CollisionDetector Class
    Description:
    This commit introduces the CollisionDetector class in collision_detector.py for handling collision detection operations at the airport.
    
    The check_collision method takes two airplanes and a limit as input. It calculates the Euclidean distance between these two airplanes and checks if it's within the given limit. If the distance is within the limit, it returns True, indicating a collision; else, it returns False.
    
    The check_all_collision method iterates over all pairs of airplanes in the airport. It checks if there is a collision between each pair using the check_collision method. If a specific airplane is given, it only checks collisions involving that airplane.
    
    If a collision is detected, it prints a warning message and returns a dictionary containing the message "collision!", and identifiers of the two colliding airplanes. If no collision is detected, it returns a dictionary with the message "No collision detected".
    
    These changes improve the airport's safety measures by allowing us to detect potential collisions and react accordingly.

---
+++ collision_detector.py
@@ -0,0 +1,33 @@
+import math
+class CollisionDetector:
+    def __init__(self, airport):
+        self.airport = airport
+
+    def check_collision(self, airplane1, airplane2, limit=10):
+        x1, y1 = airplane1.get("x"), airplane1.get("y")
+        x2, y2 = airplane2.get("x"), airplane2.get("y")
+        if not (isinstance(x1, int) and isinstance(y1, int) and isinstance(x2, int) and isinstance(y2, int)):
+            return False
+        distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+        if distance <= limit:
+            return True
+        else:
+            return False
+
+    def check_all_collision(self, limit=10, specific_airplane=None):
+        if specific_airplane is None:
+            specific_airplane_check = False
+        else:
+            specific_airplane_check = True
+
+        for i in range(len(self.airport.airplanes)):
+            for j in range(i + 1, len(self.airport.airplanes)):
+                if specific_airplane_check:
+                    if self.airport.airplanes[i] != specific_airplane and self.airport.airplanes[j] != specific_airplane:
+                        continue
+
+                if self.check_collision(self.airport.airplanes[i], self.airport.airplanes[j], limit=limit):
+                    print("Airplanes collide")
+                    message = {"message": "collision!", "airplane-1": self.airport.airplanes[i], "airplane-2": self.airport.airplanes[j]}
+                    return message
+        return {"message": "No collision detected"}
