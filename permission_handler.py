d12d1e76368d14015d7bcc6f5ee1e03afa9e984e
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-05-09 14:35:50 +0200

    Title: Add PermissionHandler Class
    Description:
    This commit introduces a new PermissionHandler class. This class encapsulates the logic related to granting permissions for landing and inbounding.
    
    The process_landing_permission_request method processes requests for landing permission.
    The grant_approach_permission method grants approach permission if the number of airplanes is less than 100.
    The handle_inbound_request and inbound_for_approach_runway methods manage the runway assignment for inbounding airplanes, ensuring that only one airplane is assigned to a runway at a time.
    The handle_inbound method delegates the handling of inbound action to the airport.
    These changes make the code more modular and improve the separation of concerns within the airport simulation.

---
+++ permission_handler.py
@@ -0,0 +1,34 @@
+class PermissionHandler:
+    def __init__(self, airport):
+        self.airport = airport
+
+    def process_landing_permission_request(self, data):
+        response = self.grant_approach_permission(data)
+        return response
+
+    def grant_approach_permission(self, data):
+        with self.airport.lock:
+            if len(self.airport.airplanes) < 100:
+                airplane = data.get("airplane_ID")
+                self.airport.airplanes.append(airplane)
+                return {"airport_message": "Permission to approach airport granted"}
+            else:
+                return False
+
+    def handle_inbound_request(self):
+        response = self.inbound_for_approach_runway()
+        return response
+
+    def inbound_for_approach_runway(self):
+        with self.airport.lock:
+            for runway in self.airport.runways:
+                if not runway.is_occupied:
+                    runway.is_occupied = True
+                    corridor_coords = runway.corridor_coords
+                    return {'message': "permission granted",
+                            "coordinates": {"x": corridor_coords[0], "y": corridor_coords[1], "z": corridor_coords[2]}}
+            return {"message": "permission denied", "data": self.airport.runways}
+
+    def handle_inbound(self, data):
+        message = self.airport.inbounding(data)
+        return message
