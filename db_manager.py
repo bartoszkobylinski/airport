b71acd7ba5ca90b3113e014428bf5b1c079c7ba0
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-07-06 11:16:11 +0200

    Title: Refactor 'DbManager' to Use Django ORM
    Description:
    This commit adapts the 'DbManager' class to the new project structure that utilizes Django's Object-Relational Mapping (ORM) system.
    
    We are transitioning from a custom SQLite setup to Django's ORM to simplify database interactions and management.
    As a part of this transition, the 'DbManager' class was simplified. The class will now default to a SQLite database file named 'db.sqlite3' in the current directory if no database name is provided.
    The 'create_database' method has been removed. Django's ORM automatically handles the creation of database tables, eliminating the need for manual table creation.
    Updated the 'add_row' method to insert data into the 'airport_app_airplane' table, adhering to Django's table naming convention.
    This refactor is part of an ongoing effort to streamline our data handling by leveraging the powerful features of Django's ORM. We now maintain a single, centralized database through Django, which will improve the efficiency of data operations.

--- db_manager.py
+++ db_manager.py
@@ -1,39 +1,17 @@
 import os
 import sqlite3
-import time
 
 
 class DbManager:
-    def __init__(self, db_name):
-        self.db_filename = db_name + ".db"
-        if not os.path.exists(self.db_filename):
-            self.create_database()
+    def __init__(self, db_name=None):
+        if db_name is None:
+            current_dir = os.path.dirname(os.path.realpath(__file__))
+            db_name = os.path.join(current_dir, "db.sqlite3")
+            self.db_filename = db_name
         self.column_names = ['airplane_id', 'x', 'y', 'z', 'fuel', 'status']
 
     def connect(self):
         return sqlite3.connect(self.db_filename)
-
-    def create_database(self):
-        connection = self.connect()
-        try:
-            cursor = connection.cursor()
-            cursor.execute('''CREATE TABLE airplanes
-                            ( id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            airplane_id TEXT,
-                            x REAL,
-                            y REAL,
-                            z REAL,
-                            fuel INTEGER,
-                            status TEXT CHECK(status IN ('WAITING',
-                                                           'APPROACHING',
-                                                           'DESCENDING',
-                                                           'LANDED',
-                                                           'CRASHED')))''')
-            connection.commit()
-        except Exception as e:
-            print(f"Exception when creating table: {e}")
-        finally:
-            connection.close()
 
     def add_row(self, **incoming_data):
         with self.connect() as connection:
@@ -42,8 +20,6 @@ class DbManager:
                 {key.lower(): value for key, value in incoming_data.items() if key.lower() in self.column_names}
             columns = ', '.join(filtered_data.keys())
             placeholders = ', '.join('?' for _ in filtered_data)
-            sql = f'INSERT INTO airplanes ({columns}) VALUES ({placeholders})'
+            sql = f'INSERT INTO airport_app_airplane ({columns}) VALUES ({placeholders})'
             cursor.execute(sql, tuple(filtered_data.values()))
             connection.commit()
-
-
